9ca7e66a8d78b4f6dd4903a2d92f52a572bacf17
Hossein Sokhandan <iamsokhandan@gmail.com>
2024-09-28 01:10:30 +0330

    add unit tests

--- src/thorax/encryptor.rs
+++ src/thorax/encryptor.rs
@@ -1,14 +1,15 @@
 use rsa::pkcs8::DecodePublicKey;
 use rsa::{Pkcs1v15Encrypt, RsaPublicKey};
+use std::path::Path;
 
 pub struct Encryptor {
-    public_key: RsaPublicKey,
+    pub public_key: RsaPublicKey,
 }
 
 impl Encryptor {
-    pub fn new(public_key: &str) -> Self {
+    pub fn new(path: impl AsRef<Path>) -> Self {
         Encryptor {
-            public_key: RsaPublicKey::from_public_key_pem(public_key)
+            public_key: RsaPublicKey::read_public_key_pem_file(path)
                 .expect("Failed to parse public key"),
         }
     }
@@ -18,5 +19,28 @@ impl Encryptor {
         self.public_key
             .encrypt(&mut rng, Pkcs1v15Encrypt, message.as_bytes())
             .expect("failed to encrypt")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::thorax::decryptor::Decryptor;
+    use std::env;
+    use dotenv::dotenv;
+
+    #[test]
+    fn test_encryption() {
+        dotenv().ok();
+
+        let encryptor: Encryptor = Encryptor::new(env::var("PUBLICKEY_PATH").unwrap());
+        let decryptor: Decryptor = Decryptor::new(env::var("PRIVATEKEY_PATH").unwrap());
+
+        let text = "fuck";
+
+        let encrypted_text = encryptor.encrypt(text);
+        let decrypted_text = decryptor.decrypt(encrypted_text.as_slice());
+
+        assert_eq!(decrypted_text, text.bytes().collect::<Vec<u8>>());
     }
 }
